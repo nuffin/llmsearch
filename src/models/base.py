d5fd96407b20b48d4c4a72bc3692e291cab14793
Hauzer S. Lee <nuffin@gmail.com>
2024-10-14 02:07:48 +0800

    chore: recover some code for backup

--- src/models/base.py
+++ src/models/base.py
@@ -1,7 +1,41 @@
 from datetime import datetime
+from sqlalchemy.ext.declarative import declared_attr
 
 from db import db
 
+
+## for backup
+class SoftDeletable(db.Model):
+    __abstract__ = True
+
+    @declared_attr
+    def deleted_at(cls):
+        return db.Column(db.DateTime, nullable=True)
+
+    def soft_delete(self):
+        self.deleted_at = datetime.utcnow()
+
+    @classmethod
+    def query_active(cls):
+        return cls.query.filter(cls.deleted_at.is_(None))
+
+
+## for backup
+class Timestamped(db.Model):
+    __abstract__ = True
+
+    @declared_attr
+    def created_at(cls):
+        return db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    @declared_attr
+    def updated_at(cls):
+        return db.Column(
+            db.DateTime,
+            nullable=False,
+            default=datetime.utcnow,
+            onupdate=datetime.utcnow,
+        )
 
 def soft_deletable(cls):
     # Directly assign `deleted_at` column
