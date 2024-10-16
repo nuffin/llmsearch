8ddde85d0bb491508a47cf0cfd5e9ff83635c26f
Hauzer S. Lee <nuffin@gmail.com>
2024-10-16 17:06:35 +0800

    chore: move test code to fileparser.mimetype

--- llmsearch/filetypes/mimetype.py
+++ llmsearch/filetypes/mimetype.py
@@ -4,5 +4,21 @@ import magic
 mime = magic.Magic(mime=True)
 
 
-def detect(filepath: str):
-    return mime.from_file(filepath) if os.path.isfile(filepath) else None
+def detect(filepath: str, follow_symlinks: bool = True) -> str:
+    resolved_filepath = follow_symlinks and os.path.realpath(filepath) or filepath
+    return mime.from_file(resolved_filepath) if os.path.isfile(filepath) else None
+
+
+def main():
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: mimetype.py <filepath>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    meme_type = detect(filepath)
+    print(f"{filepath}: {meme_type}")
+
+
+if __name__ == "__main__":
+    main()
