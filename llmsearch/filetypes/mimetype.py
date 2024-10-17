401c6c14f7f20e66f417f897a1f3a966d322c771
Hauzer S. Lee <nuffin@gmail.com>
2024-10-17 20:58:01 +0800

    chore: add some packages to requirments.txt

--- llmsearch/filetypes/mimetype.py
+++ llmsearch/filetypes/mimetype.py
@@ -3,10 +3,46 @@ import magic
 
 mime = magic.Magic(mime=True)
 
+supported_mimetypes = [
+    "text/plain",
+    "text/csv",
+    "text/html",
+    "text/xml",
+    "text/x-c",  ## c, go ## XXX: add file extension checking
+    "text/x-c++",  ## cpp
+    "text/x-java-source",  ## java
+    "text/x-go",  ## FAKE: not support by python-magic, but checking file extension
+    "text/x-matlab",  ## matlab
+    "text/x-perl",  ## perl
+    "text/x-php",  ## php
+    "text/x-python",  ## python, /etc/mime.types
+    "text/x-script.python",  ## python, by magic
+    "application/javascript",  ## js
+    "application/json",
+    # "application/x-python-code",  ## python, /etc/mime.types, pyc pyo
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",  ## pptx
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",  ## xlsx
+    "audio/mpeg",  ## mp3
+    "audio/x-wav",  ## wav
+    "image/gif",  ## gif
+    "image/jpeg",  ## jpg
+    "image/png",  ## png
+    "image/svg+xml",  ## svg
+    "video/mp4",  ## mp4
+    "video/quicktime",  ## mov
+    "video/x-msvideo",  ## avi
+]
+
 
 def detect(filepath: str, follow_symlinks: bool = True) -> str:
     resolved_filepath = follow_symlinks and os.path.realpath(filepath) or filepath
-    return mime.from_file(resolved_filepath) if os.path.isfile(filepath) else None
+    mimetype = mime.from_file(resolved_filepath) if os.path.isfile(filepath) else None
+    if mimetype in supported_mimetypes:
+        if mimetype == "text/x-c" and filepath.endswith(".go"):
+            return "text/x-go"
+        return mimetype
+    return None
 
 
 def main():
