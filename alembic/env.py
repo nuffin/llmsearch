f528a8d3d64222dd669708eedeefef972776c4cc
Hauzer S. Lee <nuffin@gmail.com>
2024-10-19 17:03:02 +0800

    chore: rename llmpa.models to llmpa schemas (#27)

--- alembic/env.py
+++ alembic/env.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine, AsyncConnection, create_async_en
 
 from alembic import context
 
-from db import db, init_models
+from db import db, init_schemas
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,7 +26,7 @@ if config.config_file_name is not None:
 
 
 ## to init db.metadata
-init_models()
+init_schemas()
 
 # add your model's MetaData object here
 # for 'autogenerate' support
